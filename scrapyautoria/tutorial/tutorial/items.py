437bf36b38b2d9f936ef25bd035c729b79c8e31c
Sergiy <sergiybednuy1993@gmail.com>
2021-10-03 16:33:49 +0300

    1.4

--- scrapyautoria/tutorial/tutorial/items.py
+++ scrapyautoria/tutorial/tutorial/items.py
@@ -9,13 +9,10 @@ import scrapy
 
 
 class TutorialItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
     model = scrapy.Field()
-    # year = year.Field()
+    year = scrapy.Field()
     mileage = scrapy.Field()
-    # price_uah = price_uah.Field()
-    # price_dollar = price_dollar.Field()
-    # vin_code = vin_code.Field()
-    # link = link.Field()
-
+    price_uah = scrapy.Field()
+    price_dollar = scrapy.Field()
+    vin_code = scrapy.Field()
+    link = scrapy.Field()
--- scrapyautoria/tutorial/tutorial/spiders/auto_ria.py
+++ scrapyautoria/tutorial/tutorial/spiders/auto_ria.py
@@ -1,38 +1,35 @@
 import scrapy
 import json
 
-from scrapyautoria.tutorial.tutorial.items import TutorialItem
+from tutorial.items import TutorialItem
 
 
 class AutoRia(scrapy.Spider):
     name = 'auto_ria'
     allowed_domains = ['auto.ria.com']
-    start_urls = [
-        'https://auto.ria.com/uk/legkovie/tesla/'
-    ]
+    start_urls = ['https://auto.ria.com/uk/legkovie/tesla/']
 
     site_url = 'https://auto.ria.com/uk/'
 
     def parse(self, response):
 
         for car in response.css('div.content'):
-            model = car.css('div > div > span.blue.bold::text').get()
-            # 'year' : cars.css('div > item ticket-title > a.span::text').get(),
-            mileage = car.css('div > ul > li > i.icon-mileage::text').get()
-            # 'price_uah' : cars.css('div > item ticket-title > a.span::text').get(),
-            # 'price_dollar' : cars.css('div > item ticket-title > a.span::text').get(),
-            # 'vin_code' : cars.css('div > item ticket-title > a.span::text').get(),
-            # 'link' : cars.css('div > item ticket-title > a.span::text').get(),
+            model = car.css('div.head-ticket > div > a > span > font > font::text').get()
+            year = car.css('div.head-ticket > div > a > font > font::text').get()
+            mileage = car.css('div.definition-data > ul > li.item-char.js-race > font > font::text').get()
+            price_uah = car.css('div.price-ticket > span > span.i-block > span > font > font::text').get()
+            price_dollar = car.css('div.price-ticket > span > span:nth-child(1) > font > font::text').get()
+            vin_code = car.css('div.definition-data > div > span.label-vin > span:nth-child(2) > font > font::text').get()
+            link = car.css('div.head-ticket > div > a::text').get()
 
             car_item = TutorialItem()
             car_item['model'] = model.strip()
-            # car_item['year'] = year.strip()
+            car_item['year'] = year.strip()
             car_item['mileage'] = mileage.strip()
-            # car_item['price_uah'] = price_uah.strip()
-            # car_item['price_dollar'] = price_dollar.strip()
-            # car_item['vin_code'] = vin_code.strip()
-            # car_item['link'] = link.strip()
-
+            car_item['price_uah'] = price_uah.strip()
+            car_item['price_dollar'] = price_dollar.strip()
+            car_item['vin_code'] = vin_code.strip() if vin_code else None
+            car_item['link'] = link.strip()
 
             next_page = response.css('span.page-item.next.text-r a::attr(href)').get()
             if next_page is not None:
